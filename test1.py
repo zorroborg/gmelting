ee929cd405d8e04025b20737d5eb16af64df4cb7
Zorroborg <zoroborg@yahoo.com>
2015-10-05 12:35:35 -0500

    add test1.py

---
+++ test1.py
@@ -0,0 +1,8 @@
+print "\n"
+age = raw_input('how old are you?\t')
+print "\n"
+
+weigh = raw_input('how much do you weigh?\t')
+print '\n'
+print "So, you're %r old and %r fat?" % (age , weigh)
+print '\n'
